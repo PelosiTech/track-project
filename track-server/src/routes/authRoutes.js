b3fd252aaec92bc499aaae312c8dbcc7d8940b5a
Carlo Pelosi <cpelosi@asu.edu>
2020-12-25 22:52:28 -0700

    add a user schema an define it

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
 const express = require('express')
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 
 const router = express.Router();
 
 router.post('/signup', (req, res) => {
     console.log(req.body)
     res.send('You made a post request')
-})
+});
 
 module.exports = router;
