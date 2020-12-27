d7344c7d5374ed324937e17c5da86dad89f8cd65
Carlo Pelosi <cpelosi@asu.edu>
2020-12-26 23:04:45 -0700

    add jwt token and send it as response

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const express = require('express')
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const User = mongoose.model('User');
 
 const router = express.Router();
@@ -11,11 +12,14 @@ router.post('/signup', async (req, res) => {
     try {
         const user = new User({ email, password });
         await user.save();
+
+        const token = jwt.sign({userId: user._id}, 'MY_SECRET_KEY');
+        res.send({token});
     } catch (err) {
         return res.status(422).send(err.message);
     }
 
-    res.send('You made a post request');
+    
 });
 
 module.exports = router;
